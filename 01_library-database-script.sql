7caa7fd9d047ff2a3e0f9d8875bd6e239cabc913
stefanus_yudi_irwan <yudi.stefanus22@gmail.com>
2022-07-14 22:40:23 +0800

    project begin - first upload

---
+++ 01_library-database-script.sql
@@ -0,0 +1,57 @@
+DROP DATABASE library;
+CREATE DATABASE IF NOT EXISTS library;
+USE library;
+
+# Table to store user info
+CREATE TABLE IF NOT EXISTS lib_user(
+	id_user INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    date_of_birth DATE,
+    occupation VARCHAR(30),
+    domicile VARCHAR(30),
+    registration_date DATE,
+    CONSTRAINT pk_user PRIMARY KEY (id_user)
+);
+
+# Table to store book info
+CREATE TABLE IF NOT EXISTS book(
+	id_book INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    year_published YEAR,
+    pages INT NOT NULL,
+    _language ENUM('Bahasa','English','Deutch','Japan'),
+    author VARCHAR(40),
+	category ENUM('novel','comic','dictionary','scientific','biography','encyclopedia','thesis','magazine','history'),
+    stock INT NOT NULL,
+    CONSTRAINT pk_book PRIMARY KEY (id_book)               
+)AUTO_INCREMENT=1000;
+
+# Table to store loan info
+CREATE TABLE IF NOT EXISTS loan(
+	id_user INT, 
+    id_book INT,
+    user_first_name VARCHAR(20),
+    user_last_name VARCHAR(20),
+    book_title VARCHAR(100),
+    loan_date DATE,    # will be filled with default date of loan_function called
+    supposed_return_date DATE,    # will be filled with loan_duration_function
+    returned ENUM('YES','NOT YET'),    # will be filled with return_function
+    returned_date DATE,    # will be filled with default date of return_function called
+    CONSTRAINT pk_loan PRIMARY KEY (id_user, id_book),
+    CONSTRAINT fk_id_user FOREIGN KEY (id_user) REFERENCES lib_user(id_user),
+	CONSTRAINT fk_id_book FOREIGN KEY (id_book) REFERENCES book(id_book)
+);
+
+# function loan_days ==> return how many days should the book borrowed
+
+# procedure show_list_user ==> procedure to get list of user
+# procedure show_book ==> procedure to get list of book
+# procedure show_loan ==> procedure to get list of loan
+
+# procedure search_user ==> procedure to show data about certain user
+# procedure seach_book ==> procedure to show data about certain book
+
+# procedure loan_book ==> procedure to fill table loan
+# procedure return_book ==> procedure to update table loan
+# procedure exit_user ==> procedure to update table user, delete user
---
+++ README.md
@@ -0,0 +1,1 @@
+This is project about library management system
